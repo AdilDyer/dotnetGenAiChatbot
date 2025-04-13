d74dc08d1da0bb27a9fb5ca3442b8616796dabc9
ADIL DYER <smile.itsadil@gmail.com>
2025-04-13 20:23:24 +0530

    frontend update

--- aichatdotnet/obj/Debug/net8.0/aichatdotnet.AssemblyInfo.cs
+++ aichatdotnet/obj/Debug/net8.0/aichatdotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aichatdotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393106fb84f5fb357b8e18af5d4631390b17003a")]
 [assembly: System.Reflection.AssemblyProductAttribute("aichatdotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aichatdotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
